d81b54717d99838ba9d2cc6ed4cacd1ea3d79927
sbraco <steve.braco@sogeti.com>
2024-11-19 14:48:27 +0100

    continue page

--- js/index.js
+++ js/index.js
@@ -25,3 +25,26 @@ btnMedical.addEventListener('click', () => {
   btnConsultation.classList.add('btn-disabled')
   btnMedical.classList.remove('btn-disabled')
 });
+
+
+
+  window.addEventListener('DOMContentLoaded', () => {
+  const observer = new IntersectionObserver(entries => {
+    console.log(entries)
+    entries.forEach(entry => {
+      const id = entry.target.getAttribute('id');
+      console.log(id)
+      if (entry.intersectionRatio > 0) {
+        document.querySelector(`.section-nav li a[href="#${id}"]`).parentElement.classList.add('active');
+      } else {
+        document.querySelector(`.section-nav li a[href="#${id}"]`).parentElement.classList.remove('active');
+      }
+    });
+  });
+
+  // Track all sections that have an `id` applied
+  document.querySelectorAll('section[id]').forEach((section) => {
+    observer.observe(section);
+  });
+  
+});
