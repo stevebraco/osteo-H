6b826077476b8554059e1c97793324fcb732a4c0
Steve Braco <stevebraco@MacBook-Air-de-Steve.local>
2024-12-06 20:58:14 +0100

    delete script in html

--- js/index.js
+++ js/index.js
@@ -20,23 +20,3 @@ btnClose.addEventListener('click',  () => closeMenu(btnMenu, btnClose))
 document.addEventListener('DOMContentLoaded', aos);
 
 document.addEventListener('scroll', scrollTop);
-
-
-//   window.addEventListener('DOMContentLoaded', () => {
-//   const observer = new IntersectionObserver(entries => {
-//     entries.forEach(entry => {
-//       const id = entry.target.getAttribute('id');
-//       if (entry.intersectionRatio > 0) {
-//         document.querySelector(`.section-nav li .section-nav-link[href="#${id}"]`).parentElement.classList.add('active');
-//       } else {
-//         document.querySelector(`.section-nav li .section-nav-link[href="#${id}"]`).parentElement.classList.remove('active');
-//       }
-//     });
-//   });
-
-//   // Track all sections that have an `id` applied
-//   document.querySelectorAll('article[id]').forEach((section) => {
-//     observer.observe(section);
-//   });
-  
-// });
