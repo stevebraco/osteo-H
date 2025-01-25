64c09ef13801d9121468966499e390596fa94808
Steve Braco <stevebraco@MacBook-Air-de-Steve.local>
2025-01-26 00:35:09 +0100

    new page article

--- js/index.js
+++ js/index.js
@@ -3,38 +3,69 @@ import { showConsultation, showMedical } from "./consultation_footer.js";
 import { closeMenu, openMenu } from "./menu.js";
 import scrollTop from "./scroll.js";
 
-const btnConsultation = document.querySelector('#consultation')
-const btnMedical = document.querySelector('#medical')
-const btnMenu = document.querySelector('.menu')
-const btnClose = document.querySelector('.menu-close')
+const btnConsultation = document.querySelector("#consultation");
+const btnMedical = document.querySelector("#medical");
+const btnMenu = document.querySelector(".menu");
+const btnClose = document.querySelector(".menu-close");
 
+btnConsultation.addEventListener("click", () =>
+  showConsultation(btnConsultation, btnMedical)
+);
+btnMedical.addEventListener("click", () =>
+  showMedical(btnConsultation, btnMedical)
+);
 
+btnMenu.addEventListener("click", () => openMenu(btnMenu, btnClose));
 
+btnClose.addEventListener("click", () => closeMenu(btnMenu, btnClose));
 
-btnConsultation.addEventListener('click', () => showConsultation(btnConsultation, btnMedical));
-btnMedical.addEventListener('click', () => showMedical(btnConsultation, btnMedical));
+document.addEventListener("DOMContentLoaded", aos);
 
-btnMenu.addEventListener('click', () => openMenu(btnMenu, btnClose))
+document.addEventListener("scroll", scrollTop);
 
-btnClose.addEventListener('click',  () => closeMenu(btnMenu, btnClose))
-
-document.addEventListener('DOMContentLoaded', aos);
-
-document.addEventListener('scroll', scrollTop);
-
-document.addEventListener('DOMContentLoaded', function () {
-  new Glide('.glide', {
-    type: 'carousel',      // Type de carrousel
-    startAt: 0,            // L'index du premier slide
-    perView: 3,            // Nombre de slides visibles à la fois (par défaut pour les écrans plus larges)
-    gap: 0,                // Espace entre les slides
-    autoplay: 2000,        // Défilement automatique toutes les 3 secondes (3000 ms)
-    hoverpause: false,     // Arrêter l'autoplay au survol
-    loop: true,            // Faire tourner les slides en boucle
+document.addEventListener("DOMContentLoaded", function () {
+  new Glide(".glide-1", {
+    type: "carousel", // Type de carrousel
+    startAt: 0, // L'index du premier slide
+    perView: 3, // Nombre de slides visibles à la fois (par défaut pour les écrans plus larges)
+    gap: 10, // Espace entre les slides
+    // autoplay: 2000, // Défilement automatique toutes les 3 secondes (3000 ms)
+    hoverpause: false, // Arrêter l'autoplay au survol
+    loop: true, // Faire tourner les slides en boucle
     breakpoints: {
       785: {
-        perView: 1,        // À partir de 785px, afficher une seule image
-      }
-    }
+        perView: 1, // À partir de 785px, afficher une seule image
+      },
+    },
+  }).mount();
+
+  new Glide(".glide-2", {
+    type: "carousel", // Type de carrousel
+    startAt: 0, // L'index du premier slide
+    perView: 3, // Nombre de slides visibles à la fois (par défaut pour les écrans plus larges)
+    gap: 10, // Espace entre les slides
+    // autoplay: 2000, // Défilement automatique toutes les 3 secondes (3000 ms)
+    hoverpause: false, // Arrêter l'autoplay au survol
+    loop: true, // Faire tourner les slides en boucle
+    breakpoints: {
+      785: {
+        perView: 1, // À partir de 785px, afficher une seule image
+      },
+    },
+  }).mount();
+
+  new Glide(".glide-3", {
+    type: "carousel", // Type de carrousel
+    startAt: 0, // L'index du premier slide
+    perView: 3, // Nombre de slides visibles à la fois (par défaut pour les écrans plus larges)
+    gap: 10, // Espace entre les slides
+    // autoplay: 2000, // Défilement automatique toutes les 3 secondes (3000 ms)
+    hoverpause: false, // Arrêter l'autoplay au survol
+    loop: true, // Faire tourner les slides en boucle
+    breakpoints: {
+      785: {
+        perView: 1, // À partir de 785px, afficher une seule image
+      },
+    },
   }).mount();
 });
